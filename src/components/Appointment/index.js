30ec9e4e34e454acdec6e8151a4a9f9bba734c03
John <jzlowie@gmail.com>
2021-07-11 16:43:08 -0600

    Added editing appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -15,6 +15,7 @@ const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
 
@@ -49,7 +50,7 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onEdit={() => props.onEdit}
+          onEdit={() => transition(EDIT)}
           onDelete={() => transition(CONFIRM)}
         />
       )}
@@ -68,6 +69,15 @@ export default function Appointment(props) {
           message="Are you sure you would like to delete?"
           onCancel={back}
           onConfirm={deleteAppointment}
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          name={props.interview.student}
+          interviewers={props.interviewers}
+          interviewer={props.interview.interviewer.id}
+          onSave={save}
+          onCancel={back}
         />
       )}
     </article>
